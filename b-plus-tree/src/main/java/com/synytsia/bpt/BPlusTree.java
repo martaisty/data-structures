664d669140dc0c646129c6edf9d9bb8976f1478e
Kostiantyn Synytsia <20212876+martaisty@users.noreply.github.com>
2024-11-12 02:41:03 +0200

    b-plus-tree: add docs

--- b-plus-tree/src/main/java/com/synytsia/bpt/BPlusTree.java
+++ b-plus-tree/src/main/java/com/synytsia/bpt/BPlusTree.java
@@ -4,14 +4,30 @@ import com.synytsia.exceptions.ExerciseNotCompletedException;
 
 public class BPlusTree {
 
+    /**
+     * @param order order of tree
+     * @throws IllegalArgumentException when order is less than 3
+     */
     public BPlusTree(final int order) {
         throw new ExerciseNotCompletedException();
     }
 
+    /**
+     * Searches key in tree, returns it if found or null otherwise
+     *
+     * @param key key to find
+     * @return key if found in tree, {@literal null} otherwise
+     */
     public Integer get(Integer key) {
         throw new ExerciseNotCompletedException();
     }
 
+    /**
+     * Inserts key into tree. Duplicates are not supported
+     *
+     * @param key new key to insert
+     * @return {@literal true} if key is inserted, {@literal false} if it's already in the tree
+     */
     public boolean insert(Integer key) {
         throw new ExerciseNotCompletedException();
     }
